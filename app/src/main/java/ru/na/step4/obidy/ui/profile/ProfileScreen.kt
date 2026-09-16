cde9461491a657e6d8a8370868d9311fc01b936e
sshell72dev <sshell72@yandex.ru>
2026-09-16 15:31:51 +0500

    Fix clickable import in ProfileScreen

--- app/src/main/java/ru/na/step4/obidy/ui/profile/ProfileScreen.kt
+++ app/src/main/java/ru/na/step4/obidy/ui/profile/ProfileScreen.kt
@@ -1,6 +1,7 @@
 package ru.na.step4.obidy.ui.profile
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -42,7 +43,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.clickable
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import java.time.Instant
